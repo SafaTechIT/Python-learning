3464cde2d735b70e9abfa52b27443086bd5bb3cc
Haj Rezvan <rezvanianragahi2@gmail.com>
2024-08-08 21:33:28 +0330

    Update README.md

--- arr.py
+++ arr.py
@@ -2,9 +2,11 @@ import random as rd
 import matplotlib.pyplot as plt
 
 
-random_numbers = [rd.randint(1, 100000) for _ in range(10000)]
+otp_array = [] # OTP Array آبرودی جان پرش کن
+id_array = [] # ID Array آبرودی جان پرش کن
 plt.figure(figsize=(12, 6))  # Set the figure size for better visibility
-plt.hist(random_numbers, bins=100, color='skyblue', edgecolor='black')
-plt.title('Distribution of Random Numbers Between 1 and 100000')
+plt.scatter(y=otp_array, x=id_array, color='skyblue', edgecolor='black')
+plt.title('Distribution of Random Numbers Between 1000 and 9999')
 plt.xlabel('Random Number Value')
 plt.ylabel('Frequency')
+plt.show()
